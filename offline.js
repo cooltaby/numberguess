270cafc60994670ca64bc4486c54137001a9ed88
cooltaby <cooltaby@users.noreply.github.com>
2019-12-30 14:06:12 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1577693478,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/numbercard-sheet0.png",
+		"images/numbercard-sheet1.png",
+		"images/numbercard-sheet2.png",
+		"images/background2000-sheet0.png",
+		"images/buttonithought-sheet0.png",
+		"images/buttonno-sheet0.png",
+		"images/buttonyes-sheet0.png",
+		"images/thinknumber-sheet0.png",
+		"media/success.m4a",
+		"media/success.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"loading-logo.png",
+		"icon-256.png",
+		"t_en.json",
+		"t_ko.json"
+	]
+}
